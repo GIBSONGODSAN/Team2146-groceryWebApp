f91d37be4e27d3aa9033edc2c8da921b82c74f82
GIBSONGODSAN <gibson.25cs@licet.ac.in>
2024-12-21 15:28:23 +0530

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const connectDB = async () => {
+  try {
+    console.log(process.env.MONGO_URI)
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    })
+
+    
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold)
+    process.exit(1)
+  }
+}
+
+export default connectDB
